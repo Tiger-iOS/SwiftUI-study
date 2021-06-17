45048389925706d2a4c73bb233849ec7183ab480
Tiger <tiger_ios@163.com>
2021-06-17 10:18:04 +0800

    项目创建

---
+++ SwiftUI-S/SwiftUI_SApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SApp.swift
+//  SwiftUI-S
+//
+//  Created by imac on 2021/6/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
